005375305851c0f00deb5b3e815a1af3814c24ca
Claire Froelich <claire.froelich@gmail.com>
2024-07-29 14:57:05 -0400

    make typescript SHUT UP

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -14,6 +14,7 @@ module.exports = {
   rules: {
     "react/jsx-no-target-blank": "off",
     "react/prop-types": "off",
+    "react/no-unescaped-entities": "off",
     "react-refresh/only-export-components": [
       "warn",
       { allowConstantExport: true },
