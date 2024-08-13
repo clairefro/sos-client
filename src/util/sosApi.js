38eb17cdcf215a25e621176fba2ff4fc92f267ca
Claire Froelich <claire.froelich@gmail.com>
2024-08-13 13:14:43 -0400

    Refactor API client, add 'reply' utility

--- src/util/sosApi.js
+++ src/util/sosApi.js
@@ -5,10 +5,26 @@ const SosApi = {
   generateThead: async function (question) {
     const URL = `${config.SOS_API_BASE_URL}/generate/thread`;
 
-    const data = { question };
+    return await this.call({
+      url: URL,
+      data: { question },
+      method: "post",
+    });
+  },
 
+  generateReply: async function (messages) {
+    const URL = `${config.SOS_API_BASE_URL}/generate/reply`;
+
+    return await this.call({
+      url: URL,
+      data: { messages },
+      method: "post",
+    });
+  },
+
+  call: async function ({ method, url, data }) {
     try {
-      const response = await axios.post(URL, data);
+      const response = await axios({ url, method, data });
       return response.data;
     } catch (error) {
       console.error("Error calling SOS API:", error);
