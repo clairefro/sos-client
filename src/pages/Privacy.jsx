e1ce4f9f33338c2aff53f1feed955acdc03a9f6c
Claire Froelich <claire.froelich@gmail.com>
2024-09-02 11:26:36 -0400

    Use mdParser for privacy page

--- src/pages/Privacy.jsx
+++ src/pages/Privacy.jsx
@@ -1,88 +1,41 @@
+import { mdParser } from "../util/md/mdParser";
+
+const md = `
+# Privacy Policy
+
+Your privacy is important. This privacy policy explains what data we collect, how we use it, and your options when using this app.
+
+### 1. Data we collect or store
+- We do **not** store any personal information or user data.
+- We only monitor the **timestamp** and **cost** when questions are asked, anonymously, to help us understand app usage.
+- Anonymous aggregate usage data is stored in your local browser to display your own usage to you.
+
+### 2. What we do not collect or store
+-  We do **not** save or log your questions or responses.
+
+### 3. Data sent to OpenAI
+- When you use this app, **any question or reply you submit is sent to OpenAI to generate answers**.
+- By using this app, you agree to share your inputs and generated answers with OpenAI.
+- Therefore, **be mindful about submitting sensitive data**, such as proprietary code or private information.
+- To learn more about how OpenAI handles your data, please refer to the [OpenAI Privacy Policy](https://openai.com/policies/privacy-policy/)
+
+### 4. Running the app locally 
+- For more control over your data, you can **run the app locally** with your own OpenAI API Key and language model of choice ([how to run app locally](https://github.com/clairefro/sos-client?tab=readme-ov-file#devlopment-and-running-locally)).
+
+### 5. Changes to This Policy
+- We may update this privacy policy from time to time, but **will never expose your questions or answers to any party but OpenAI**
+
+### Questions?
+Message [@clairefroe on X](https://x.com/clairefroe) if you have any questions about this policy.
+`;
+
 function Privacy() {
   return (
     <div className="info-page">
-      <h1>Privacy Policy</h1>
-      <p>
-        Your privacy is important. This privacy policy explains what data we
-        collect, how we use it, and your options when using this app.
-      </p>
-      <h3>1. Data We Collect or Store</h3>
-      <ul>
-        <li>
-          We do <strong>not</strong> store any personal information or user
-          data.
-        </li>
-        <li>
-          We only monitor the <strong>timestamp</strong> and{" "}
-          <strong>cost</strong> when questions are asked, anonymously, to help
-          us understand app usage.
-        </li>
-        <li>Anonymous usage data is stored in your local browser</li>
-      </ul>
-      <h3>2. Data Sent to OpenAI</h3>
-      <ul>
-        <li>
-          When you use this app, any information you submit is sent to OpenAI to
-          generate answers.
-        </li>
-        <li>
-          By using the app,{" "}
-          <strong>you agree to share your data with OpenAI.</strong>
-        </li>
-        <li>
-          {" "}
-          To learn more about how OpenAI handles your data, please refer to the{" "}
-          <a
-            href="https://openai.com/policies/privacy-policy/"
-            target="_blank"
-            rel="noopener noreffer"
-          >
-            OpenAI Privacy Policy
-          </a>
-          .
-        </li>
-        <li>
-          For this reason, <strong>do not submit any sensitive data</strong>{" "}
-          such as proprietary code or private information
-        </li>
-        <li>
-          For more control over your data, you can download and run the app
-          locally with your own OpenAI API Key or use a different language model
-          (
-          <a
-            href="https://github.com/clairefro/sos-client?tab=readme-ov-file#devlopment-and-running-locally"
-            target="_blank"
-            rel="noopener noreffer"
-          >
-            instructions
-          </a>
-          )
-        </li>
-      </ul>
-      <h3>3. What We Do Not Collect or Store</h3>
-      <ul>
-        <li>
-          We do <strong>not</strong> save or log your questions or responses.
-        </li>
-      </ul>
-      <h3>4. Changes to This Policy</h3>
-      <ul>
-        <li>
-          We may update this privacy policy from time to time, but will never
-          expose your questions or answers to any party but OpenAI.{" "}
-        </li>
-      </ul>
-      <h3>Questions?</h3>
-      <p>
-        If you have any questions about this policy, message{" "}
-        <a
-          href="https://x.com/clairefroe"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          @clairefroe on X
-        </a>
-      </p>
+      <div
+        className="content-box-body"
+        dangerouslySetInnerHTML={{ __html: mdParser.render(md) }}
+      ></div>
     </div>
   );
 }
