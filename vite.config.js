32b50cfe428bdb842b55f1eb1ce4d140391c7a14
Claire Froelich <claire.froelich@gmail.com>
2024-11-05 13:37:22 -0500

    Improve production performance

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,14 @@ import { visualizer } from "rollup-plugin-visualizer";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), visualizer({ filename: "dist/stats.html" })],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          // Split `gpt-tokens` into a separate chunk for performance (5.6M)
+          "gpt-tokens": ["gpt-tokens"],
+        },
+      },
+    },
+  },
 });
